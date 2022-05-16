8d2ca678d973501b60479a8dc3f212eecd56eab8
Gildas Le Corguillé <lecorguille@users.noreply.github.com>
2022-05-16 11:05:34 +0200

    Mixmodel4repeated measures (#185)
    * First commit
    
    * update_xml
    
    update of the xml description of the tools part.
    
    * Use_trycatch
    
    Using tryCatch function to avoid stop processing ig for loop.
    
    * Reorganization of files
    
    * Removal of useless libraries
    
    * Correction of bugs
    
    * Creation of the .yml file
    
    * Reorganization of files for toolsehd upload
    
    * useless runit delete
    
    * Kuznestsova
    
    * pin deps versions
    
    * fix syntax ; add citations
    
    * add a functional test  ; add a .travis.yml config file
    
    * update the test_data output to fit with the precision
    
    * update the test-data/mixmodel_demo1_variables.txt, generated from a Linux (not a MacOSX)
    
    * rename the file .shed to .shed.yml
    
    * add estimates plots
    
    Estimates plots added taking into account of the pvalue threshold defined by user.
    
    * modif_lmerTest_V3
    
    modification due to the new version of package lmerTest
    
    * update deps
    
    * update test data
    
    * change owner
    
    * version 1.0.0
    
    * onlyOneRepeatedFactor
    
    New functionnality : allow model with just a repeated factor (time for example).
    Results outputs are improved for better readability and interpretation.
    
    * mixmodel4repeated_measures - prepare to migration
    
    * mixmodel4repeated_measures - migration step2
    
    * mixmodel4repeated_measures - some little fix
    
    * fix deps, simple quote
    
    * fix deps version
    
    * Modofication of test files and xml for test execution
    
    * correction of xml test paramters
    
    * Last updated version from JFM
    
    * Fix of XML regarding test lines
    
    * Additional fixes in XML
    
    * A few lintr-suggested changes
    
    * Not-to-forget update of XML (maintainers, version)
    
    * Lintr-suggested: a few spaces around infix operators
    
    * The tool now handle models without fixed factor
    
    * Adding details about plots
    
    * A little more of lintr space handling
    
    * Comments and log messages review
    
    * Still lintr-ing scripts
    
    * Commented code handling
    
    * Still lintr-ing scripts
    
    * Still lintr-ing
    
    * Still lintr-ing
    
    * Stil lintr-ing
    
    * Still lintr-ing (almost finished)
    
    * One more lintr commit
    
    * Adding the dfff method choice
    
    * Remove unnecessary versions of dependences
    
    * A minor addition in logs
    
    * Removing dependences of packages included in R-base
    
    * Finalisation of dependency review
    
    * Not using the batch package anymore
    
    * Update of expected output for test
    
    * Handling of impossible computation for models
    
    * Minor info.txt messages update
    
    * Adding diagnostics' pdf in history only when required
    
    * Correct output file update...
    
    * Adding a parameter to choose a rounding
    
    * "new line" guessing in test output
    
    * single-code fixing
    
    * adding the expect_num_outputs attribute in test
    
    * fixing the expect_num argument
    
    * switching from stdio to detect_errors
    
    * version for the lme4 R package
    
    * tool id update and addition of a profile argument
    
    Co-authored-by: jfmartin <jfmartin@toulouse.inra.fr>
    Co-authored-by: Jean-Francois Martin <jean-francois.martin@inra.fr>
    Co-authored-by: INRA\mtremblayfr <marie.tremblay-franco@inra.fr>
    Co-authored-by: Jean-François Martin <jean-francois.martin@inrae.fr>
    Co-authored-by: melpetera <melanie.petera@inrae.fr>

---
+++ tools/mixmodel4repeated_measures/mixmodel_wrapper.R
@@ -0,0 +1,200 @@
+#!/usr/bin/env Rscript
+
+library(lme4)     ## mixed model computing
+library(Matrix)
+library(MASS)
+library(lmerTest) ## computing pvalue and lsmeans from results of lme4 package
+library(multtest) ## multiple testing
+library(ggplot2)
+library(gridExtra)
+library(grid)
+
+source_local <- function(fname) {
+    argv <- commandArgs(trailingOnly = FALSE)
+    base_dir <- dirname(substring(argv[grep("--file=", argv)], 8))
+    source(paste(base_dir, fname, sep = "/"))
+}
+
+source_local("mixmodel_script.R")
+source_local("diagmfl.R")
+
+parse_args <- function() {
+  args <- commandArgs()
+  start <- which(args == "--args")[1] + 1
+  if (is.na(start)) {
+    return(list())
+  }
+  seq_by2 <- seq(start, length(args), by = 2)
+  result <- as.list(args[seq_by2 + 1])
+  names(result) <- args[seq_by2]
+  return(result)
+}
+
+argVc <- unlist(parse_args())
+
+##------------------------------
+## Initializing
+##------------------------------
+
+## options
+##--------
+
+strAsFacL <- options()$stringsAsFactors
+options(stringsAsFactors = FALSE)
+
+## constants
+##----------
+
+modNamC <- "mixmodel" ## module name
+
+topEnvC <- environment()
+flagC <- "\n"
+
+## functions
+##----------
+
+flgF <- function(tesC,
+                 envC = topEnvC,
+                 txtC = NA) { ## management of warning and error messages
+
+    tesL <- eval(parse(text = tesC), envir = envC)
+
+    if (!tesL) {
+
+        sink(NULL)
+        stpTxtC <- ifelse(is.na(txtC),
+                          paste0(tesC, " is FALSE"),
+                          txtC)
+
+        stop(stpTxtC,
+             call. = FALSE)
+
+    }
+
+} ## flgF
+
+## log file
+##---------
+
+sink(argVc["information"])
+
+cat("\nStart of the '", modNamC, "' Galaxy module call: ", format(Sys.time(), "%a %d %b %Y %X"), "\n", sep = "")
+cat("\nParameters used:\n\n")
+print(argVc)
+cat("\n\n")
+
+## loading
+##--------
+
+datMN <- t(as.matrix(read.table(argVc["dataMatrix_in"],
+                                check.names = FALSE,
+                                header = TRUE,
+                                row.names = 1,
+                                sep = "\t")))
+
+samDF <- read.table(argVc["sampleMetadata_in"],
+                    check.names = FALSE,
+                    header = TRUE,
+                    row.names = 1,
+                    sep = "\t")
+
+varDF <- read.table(argVc["variableMetadata_in"],
+                    check.names = FALSE,
+                    header = TRUE,
+                    row.names = 1,
+                    sep = "\t")
+
+
+## checking
+##---------
+
+flgF("identical(rownames(datMN), rownames(samDF))", txtC = "Column names of the dataMatrix are not identical to the row names of the sampleMetadata; check your data with the 'Check Format' module in the 'Quality Control' section")
+flgF("identical(colnames(datMN), rownames(varDF))", txtC = "Row names of the dataMatrix are not identical to the row names of the variableMetadata; check your data with the 'Check Format' module in the 'Quality Control' section")
+
+flgF("argVc['time']    %in% colnames(samDF)", txtC = paste0("Required time factor '", argVc["time"], "' could not be found in the column names of the sampleMetadata"))
+flgF("argVc['subject'] %in% colnames(samDF)", txtC = paste0("Required subject factor '", argVc["subject"], "' could not be found in the column names of the sampleMetadata"))
+
+flgF("mode(samDF[, argVc['time']])    %in% c('character', 'numeric')", txtC = paste0("The '", argVc["time"], "' column of the sampleMetadata should contain either number only, or character only"))
+flgF("mode(samDF[, argVc['subject']]) %in% c('character', 'numeric')", txtC = paste0("The '", argVc["subject"], "' column of the sampleMetadata should contain either number only, or character only"))
+
+flgF("argVc['adjC'] %in% c('holm', 'hochberg', 'hommel', 'bonferroni', 'BH', 'BY', 'fdr', 'none')")
+flgF("argVc['trf'] %in% c('none', 'log10', 'log2')")
+
+flgF("0 <= as.numeric(argVc['thrN']) && as.numeric(argVc['thrN']) <= 1", txtC = "(corrected) p-value threshold must be between 0 and 1")
+flgF("argVc['diaR'] %in% c('no', 'yes')")
+
+
+##------------------------------
+## Formating
+##------------------------------
+
+if (argVc["dff"] == "Satt") {
+  dffmeth <- "Satterthwaite"
+} else {
+  dffmeth <- "Kenward-Roger"
+}
+
+
+##------------------------------
+## Computation
+##------------------------------
+
+
+varDFout <- lmixedm(datMN = datMN,
+                     samDF = samDF,
+                     varDF = varDF,
+                     fixfact     = argVc["fixfact"],
+                     time        = argVc["time"],
+                     subject     = argVc["subject"],
+                     logtr       = argVc["trf"],
+                     pvalCutof   = argVc["thrN"],
+                     pvalcorMeth = argVc["adjC"],
+                     dffOption   = dffmeth,
+                     visu        = argVc["diaR"],
+                     least.confounded = FALSE,
+                     outlier.limit = 3,
+                     pdfC        = argVc["out_graph_pdf"],
+                     pdfE        = argVc["out_estim_pdf"]
+                     )
+
+
+
+
+##------------------------------
+## Rounding
+##------------------------------
+
+if (argVc["rounding"] == "yes") {
+  varDFout[, which(!(colnames(varDFout) %in% colnames(varDF)))] <- apply(varDFout[, which(!(colnames(varDFout) %in% colnames(varDF)))], 2, round, digits = as.numeric(argVc["decplaces"]))
+}
+
+##------------------------------
+## Ending
+##------------------------------
+
+
+## saving
+##--------
+
+varDFout <- cbind.data.frame(variableMetadata = rownames(varDFout),
+                          varDFout)
+
+write.table(varDFout,
+            file = argVc["variableMetadata_out"],
+            quote = FALSE,
+            row.names = FALSE,
+            sep = "\t")
+
+## closing
+##--------
+
+cat("\n\nEnd of '", modNamC, "' Galaxy module call: ",
+    as.character(Sys.time()), "\n", sep = "")
+cat("\nInformation about R (version, Operating System, attached or loaded packages):\n\n")
+sessionInfo()
+
+sink()
+
+options(stringsAsFactors = strAsFacL)
+
+rm(list = ls())

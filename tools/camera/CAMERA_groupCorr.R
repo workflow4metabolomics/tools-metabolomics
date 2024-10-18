52883bb6004d24fb3c55ec4804f5a4ba58279866
Damien RAT <damien.rat44@gmail.com>
2024-10-18 15:31:43 +0200

    Code indentation for R files

--- tools/camera/CAMERA_groupCorr.R
+++ tools/camera/CAMERA_groupCorr.R
@@ -11,18 +11,18 @@ print("Command-line arguments retrieved:")
 print(args_vec)
 
 args <- list(
-	     image           = args_vec[1],                  # the xsAnnotate object
-	     cor_eic_th      = as.numeric(args_vec[2]),      # Correlation threshold for EIC correlation
-	     pval            = as.numeric(args_vec[3]),      # p-value threshold for testing correlation significance
-	     graphMethod     = args_vec[4],                  # Clustering method for resulting correlation graph. See calcPC for more details.
-	     calcIso         = as.logical(args_vec[5]),      # Include isotope detection information for graph clustering
-	     calcCiS         = as.logical(args_vec[6]),      # Calculate correlation inside samples
-	     calcCaS         = as.logical(args_vec[7]),      # Calculate correlation across samples
-	     cor_exp_th      = as.numeric(args_vec[8]),      # Threshold for intensity correlations across samples
-	     intval          = args_vec[9],                  # Selection of the intensity values (such as "into") used in the correlation analysis. See getPeaklist for all allowed values.
-	     numDigitsMZ     = as.numeric(args_vec[10]),     # Digits for MZ "customname" 
-	     numDigitsRT     = as.numeric(args_vec[11]),     # Digits for RT "customname"
-	     convertRTMinute = as.logical(args_vec[12])	     # TRUE - FALSE 
+  image           = args_vec[1], # the xsAnnotate object
+  cor_eic_th      = as.numeric(args_vec[2]), # Correlation threshold for EIC correlation
+  pval            = as.numeric(args_vec[3]), # p-value threshold for testing correlation significance
+  graphMethod     = args_vec[4], # Clustering method for resulting correlation graph. See calcPC for more details.
+  calcIso         = as.logical(args_vec[5]), # Include isotope detection information for graph clustering
+  calcCiS         = as.logical(args_vec[6]), # Calculate correlation inside samples
+  calcCaS         = as.logical(args_vec[7]), # Calculate correlation across samples
+  cor_exp_th      = as.numeric(args_vec[8]), # Threshold for intensity correlations across samples
+  intval          = args_vec[9], # Selection of the intensity values (such as "into") used in the correlation analysis. See getPeaklist for all allowed values.
+  numDigitsMZ     = as.numeric(args_vec[10]), # Digits for MZ "customname"
+  numDigitsRT     = as.numeric(args_vec[11]), # Digits for RT "customname"
+  convertRTMinute = as.logical(args_vec[12]) # TRUE - FALSE
 )
 
 print("Converted arguments:")
@@ -35,7 +35,7 @@ print(sapply(args, class))
 
 # Verify if the arguments are correct
 if (!file.exists(args$image)) {
-	stop("The provided RData file does not exist: ", args$image)
+  stop("The provided RData file does not exist: ", args$image)
 }
 
 # Load the RData file
@@ -44,42 +44,42 @@ args$image <- NULL
 
 # Function to retrieve the raw file from the arguments
 getRawfilePathFromArguments <- function(singlefile, zipfile, args) {
-	if (!is.null(args$zipfile)) zipfile <- args$zipfile
-	if (!is.null(args$zipfilePositive)) zipfile <- args$zipfilePositive
-	if (!is.null(args$zipfileNegative)) zipfile <- args$zipfileNegative
+  if (!is.null(args$zipfile)) zipfile <- args$zipfile
+  if (!is.null(args$zipfilePositive)) zipfile <- args$zipfilePositive
+  if (!is.null(args$zipfileNegative)) zipfile <- args$zipfileNegative
 
-	if (!is.null(args$singlefile_galaxyPath)) {
-		singlefile_galaxyPaths <- args$singlefile_galaxyPath
-		singlefile_sampleNames <- args$singlefile_sampleName
-	}
-	if (!is.null(args$singlefile_galaxyPathPositive)) {
-		singlefile_galaxyPaths <- args$singlefile_galaxyPathPositive
-		singlefile_sampleNames <- args$singlefile_sampleNamePositive
-	}
-	if (!is.null(args$singlefile_galaxyPathNegative)) {
-		singlefile_galaxyPaths <- args$singlefile_galaxyPathNegative
-		singlefile_sampleNames <- args$singlefile_sampleNameNegative
-	}
-	if (exists("singlefile_galaxyPaths")) {
-		singlefile_galaxyPaths <- unlist(strsplit(singlefile_galaxyPaths, ","))
-		singlefile_sampleNames <- unlist(strsplit(singlefile_sampleNames, ","))
+  if (!is.null(args$singlefile_galaxyPath)) {
+    singlefile_galaxyPaths <- args$singlefile_galaxyPath
+    singlefile_sampleNames <- args$singlefile_sampleName
+  }
+  if (!is.null(args$singlefile_galaxyPathPositive)) {
+    singlefile_galaxyPaths <- args$singlefile_galaxyPathPositive
+    singlefile_sampleNames <- args$singlefile_sampleNamePositive
+  }
+  if (!is.null(args$singlefile_galaxyPathNegative)) {
+    singlefile_galaxyPaths <- args$singlefile_galaxyPathNegative
+    singlefile_sampleNames <- args$singlefile_sampleNameNegative
+  }
+  if (exists("singlefile_galaxyPaths")) {
+    singlefile_galaxyPaths <- unlist(strsplit(singlefile_galaxyPaths, ","))
+    singlefile_sampleNames <- unlist(strsplit(singlefile_sampleNames, ","))
 
-		singlefile <- NULL
-		for (singlefile_galaxyPath_i in seq_len(length(singlefile_galaxyPaths))) {
-			singlefile_galaxyPath <- singlefile_galaxyPaths[singlefile_galaxyPath_i]
-			singlefile_sampleName <- singlefile_sampleNames[singlefile_galaxyPath_i]
-			singlefile[[singlefile_sampleName]] <- singlefile_galaxyPath
-		}
-	}
-	for (argument in c(
-			   "zipfile", "zipfilePositive", "zipfileNegative",
-			   "singlefile_galaxyPath", "singlefile_sampleName",
-			   "singlefile_galaxyPathPositive", "singlefile_sampleNamePositive",
-			   "singlefile_galaxyPathNegative", "singlefile_sampleNameNegative"
-			   )) {
-		args[[argument]] <- NULL
-	}
-	return(list(zipfile = zipfile, singlefile = singlefile, args = args))
+    singlefile <- NULL
+    for (singlefile_galaxyPath_i in seq_len(length(singlefile_galaxyPaths))) {
+      singlefile_galaxyPath <- singlefile_galaxyPaths[singlefile_galaxyPath_i]
+      singlefile_sampleName <- singlefile_sampleNames[singlefile_galaxyPath_i]
+      singlefile[[singlefile_sampleName]] <- singlefile_galaxyPath
+    }
+  }
+  for (argument in c(
+    "zipfile", "zipfilePositive", "zipfileNegative",
+    "singlefile_galaxyPath", "singlefile_sampleName",
+    "singlefile_galaxyPathPositive", "singlefile_sampleNamePositive",
+    "singlefile_galaxyPathNegative", "singlefile_sampleNameNegative"
+  )) {
+    args[[argument]] <- NULL
+  }
+  return(list(zipfile = zipfile, singlefile = singlefile, args = args))
 }
 
 # Save arguments to generate a report
@@ -94,33 +94,33 @@ args <- rawFilePath$args
 
 # Function to retrieve the raw file in the working directory
 retrieveRawfileInTheWorkingDir <- function(singlefile, zipfile) {
-	if (!is.null(singlefile) && (length(singlefile) > 0)) {
-		for (singlefile_sampleName in names(singlefile)) {
-			singlefile_galaxyPath <- singlefile[[singlefile_sampleName]]
-			if (!file.exists(singlefile_galaxyPath)) {
-				error_message <- paste("Cannot access the sample:", singlefile_sampleName, "located at:", singlefile_galaxyPath)
-				print(error_message)
-				stop(error_message)
-			}
-			file.symlink(singlefile_galaxyPath, singlefile_sampleName)
-		}
-		directory <- "."
-	}
-	if (!is.null(zipfile) && (zipfile != "")) {
-		if (!file.exists(zipfile)) {
-			error_message <- paste("Cannot access the Zip file:", zipfile)
-			print(error_message)
-			stop(error_message)
-		}
-		suppressWarnings(unzip(zipfile, unzip = "unzip"))
-		filesInZip <- unzip(zipfile, list = TRUE)
-		directories <- unique(unlist(lapply(strsplit(filesInZip$Name, "/"), function(x) x[1])))
-		directories <- directories[!(directories %in% c("__MACOSX")) & file.info(directories)$isdir]
-		directory <- "."
-		if (length(directories) == 1) directory <- directories
-		cat("files_root_directory\t", directory, "\n")
-	}
-	return(directory)
+  if (!is.null(singlefile) && (length(singlefile) > 0)) {
+    for (singlefile_sampleName in names(singlefile)) {
+      singlefile_galaxyPath <- singlefile[[singlefile_sampleName]]
+      if (!file.exists(singlefile_galaxyPath)) {
+        error_message <- paste("Cannot access the sample:", singlefile_sampleName, "located at:", singlefile_galaxyPath)
+        print(error_message)
+        stop(error_message)
+      }
+      file.symlink(singlefile_galaxyPath, singlefile_sampleName)
+    }
+    directory <- "."
+  }
+  if (!is.null(zipfile) && (zipfile != "")) {
+    if (!file.exists(zipfile)) {
+      error_message <- paste("Cannot access the Zip file:", zipfile)
+      print(error_message)
+      stop(error_message)
+    }
+    suppressWarnings(unzip(zipfile, unzip = "unzip"))
+    filesInZip <- unzip(zipfile, list = TRUE)
+    directories <- unique(unlist(lapply(strsplit(filesInZip$Name, "/"), function(x) x[1])))
+    directories <- directories[!(directories %in% c("__MACOSX")) & file.info(directories)$isdir]
+    directory <- "."
+    if (length(directories) == 1) directory <- directories
+    cat("files_root_directory\t", directory, "\n")
+  }
+  return(directory)
 }
 
 # Retrieve the files
@@ -141,7 +141,7 @@ RTSecondToMinute <- function(variableMetadata, convertRTMinute) {
 
 # Ensure the xa object is loaded
 if (!exists("xa")) {
-	stop("The xa object was not found in the RData file.")
+  stop("The xa object was not found in the RData file.")
 }
 
 print("xa object loaded:")
@@ -149,7 +149,7 @@ print(xa)
 
 # Apply the groupCorr function to the xsAnnotate object
 print("Calling groupCorr function:")
-xa <- groupCorr(xa, cor_eic_th=args$cor_eic_th, pval=args$pval, graphMethod=args$graphMethod, calcIso=args$calcIso, calcCiS=args$calcCiS, calcCaS=args$calcCaS, cor_exp_th=args$cor_exp_th, intval=args$intval)
+xa <- groupCorr(xa, cor_eic_th = args$cor_eic_th, pval = args$pval, graphMethod = args$graphMethod, calcIso = args$calcIso, calcCiS = args$calcCiS, calcCaS = args$calcCaS, cor_exp_th = args$cor_exp_th, intval = args$intval)
 
 print("Result of groupCorr function:")
 print(xa)
@@ -158,17 +158,17 @@ print(xa)
 peakList <- getPeaklist(xa, intval = args$intval)
 
 # Generate group names with and without decimals for mz and rt
-names_default <- groupnames(xa@xcmsSet, mzdec = 0, rtdec = 0)                                  # Names without decimals
-names_custom <- groupnames(xa@xcmsSet, mzdec = args$numDigitsMZ , rtdec = args$numDigitsRT)    # Names with "x" decimals
+names_default <- groupnames(xa@xcmsSet, mzdec = 0, rtdec = 0) # Names without decimals
+names_custom <- groupnames(xa@xcmsSet, mzdec = args$numDigitsMZ, rtdec = args$numDigitsRT) # Names with "x" decimals
 
 # Calculate indices of the columns to include from peakList
 # Select all columns except the last sample-specific columns
 ncols <- length(colnames(peakList))
-sample_cols <- length(xa@sample)   # Number of samples
+sample_cols <- length(xa@sample) # Number of samples
 
 # Indices for the columns of interest
-main_cols <- 1:(ncols - (3 + sample_cols))  # Main columns before sample columns
-tail_cols <- (ncols - 2):ncols              # The last 3 columns (adduct, isotope, pcgroup)
+main_cols <- 1:(ncols - (3 + sample_cols)) # Main columns before sample columns
+tail_cols <- (ncols - 2):ncols # The last 3 columns (adduct, isotope, pcgroup)
 
 # Combine the selected columns from matgrp with the group names
 variableMetadata <- cbind(
@@ -180,13 +180,13 @@ variableMetadata <- cbind(
 if (!exists("RTinMinute")) RTinMinute <- FALSE
 
 if (args$convertRTMinute == TRUE && RTinMinute == FALSE) {
-        RTinMinute <- TRUE
-        variableMetadata <- RTSecondToMinute(variableMetadata = variableMetadata, convertRTMinute = args$convertRTMinute)
+  RTinMinute <- TRUE
+  variableMetadata <- RTSecondToMinute(variableMetadata = variableMetadata, convertRTMinute = args$convertRTMinute)
 }
 
 # Save the extracted peak list as a TSV file named 'variableMetadata.tsv'
 output_file_tsv <- "variableMetadata.tsv"
-write.table(variableMetadata, file=output_file_tsv, sep = "\t", row.names = FALSE, quote = FALSE)
+write.table(variableMetadata, file = output_file_tsv, sep = "\t", row.names = FALSE, quote = FALSE)
 
 # Save the updated xsAnnotate object
 output_file_RData <- "camera_groupCorr.RData"
@@ -194,4 +194,3 @@ objects2save <- c("xa", "variableMetadata", "listOFlistArguments", "zipfile", "s
 save(list = objects2save[objects2save %in% ls()], file = output_file_RData)
 
 cat("Output files generated:", output_file_tsv, "and", output_file_RData, "\n")
-
--- tools/camera/CAMERA_groupFWHM.R
+++ tools/camera/CAMERA_groupFWHM.R
@@ -12,18 +12,18 @@ print(args_vec)
 
 # Convert arguments to appropriate types
 args <- list(
-	     image = args_vec[1],                  	# RData input file
-	     sigma = as.numeric(args_vec[2]),      	# Convert sigma to numeric
-	     perfwhm = as.numeric(args_vec[3]),    	# Convert perfwhm to numeric
-	     intval = args_vec[4],                 	# intval parameter ("into", "maxo", "intb")
-	     convertRTMinute = as.logical(args_vec[5]),	# TRUE - FALSE
-	     numDigitsMZ = as.numeric(args_vec[6]),	# Number of digits MZ
-	     numDigitsRT = as.numeric(args_vec[7])	# Number of digits RT
+  image           = args_vec[1], # RData input file
+  sigma           = as.numeric(args_vec[2]), # Convert sigma to numeric
+  perfwhm         = as.numeric(args_vec[3]), # Convert perfwhm to numeric
+  intval          = args_vec[4], # intval parameter ("into", "maxo", "intb")
+  convertRTMinute = as.logical(args_vec[5]), # TRUE - FALSE
+  numDigitsMZ     = as.numeric(args_vec[6]), # Number of digits MZ
+  numDigitsRT     = as.numeric(args_vec[7]) # Number of digits RT
 )
 
 # Check if the image file exists
 if (!file.exists(args$image)) {
-	stop("The RData file does not exist: ", args$image)
+  stop("The RData file does not exist: ", args$image)
 }
 
 # Load the RData file (it should contain the xset object, typically an xcmsSet or XCMSnExp)
@@ -32,73 +32,73 @@ args$image <- NULL
 
 # Function to get an xcmsSet or XCMSnExp object
 getxcmsSetObject <- function(xobject) {
-	# XCMS 1.x
-	if (class(xobject) == "xcmsSet") {
-		return(xobject)
-	}
-	# XCMS 3.x
-	if (class(xobject) == "XCMSnExp") {
-		suppressWarnings(xset <- as(xobject, "xcmsSet"))
-		if (is.null(xset@phenoData$sample_group)) {
-			sampclass(xset) <- "."
-		} else {
-			sampclass(xset) <- xset@phenoData$sample_group
-		}
-		if (!is.null(xset@phenoData$sample_name)) {
-			rownames(xset@phenoData) <- xset@phenoData$sample_name
-		}
-		return(xset)
-	}
+  # XCMS 1.x
+  if (class(xobject) == "xcmsSet") {
+    return(xobject)
+  }
+  # XCMS 3.x
+  if (class(xobject) == "XCMSnExp") {
+    suppressWarnings(xset <- as(xobject, "xcmsSet"))
+    if (is.null(xset@phenoData$sample_group)) {
+      sampclass(xset) <- "."
+    } else {
+      sampclass(xset) <- xset@phenoData$sample_group
+    }
+    if (!is.null(xset@phenoData$sample_name)) {
+      rownames(xset@phenoData) <- xset@phenoData$sample_name
+    }
+    return(xset)
+  }
 }
 
 # If the xdata object exists, convert it to xcmsSet
 if (exists("xdata")) {
-	xset <- getxcmsSetObject(xdata)
+  xset <- getxcmsSetObject(xdata)
 }
 
 # Function to retrieve the raw file from the arguments
 getRawfilePathFromArguments <- function(singlefile, zipfile, args) {
-	if (!is.null(args$zipfile)) zipfile <- args$zipfile
-	if (!is.null(args$zipfilePositive)) zipfile <- args$zipfilePositive
-	if (!is.null(args$zipfileNegative)) zipfile <- args$zipfileNegative
+  if (!is.null(args$zipfile)) zipfile <- args$zipfile
+  if (!is.null(args$zipfilePositive)) zipfile <- args$zipfilePositive
+  if (!is.null(args$zipfileNegative)) zipfile <- args$zipfileNegative
 
-	if (!is.null(args$singlefile_galaxyPath)) {
-		singlefile_galaxyPaths <- args$singlefile_galaxyPath
-		singlefile_sampleNames <- args$singlefile_sampleName
-	}
-	if (!is.null(args$singlefile_galaxyPathPositive)) {
-		singlefile_galaxyPaths <- args$singlefile_galaxyPathPositive
-		singlefile_sampleNames <- args$singlefile_sampleNamePositive
-	}
-	if (!is.null(args$singlefile_galaxyPathNegative)) {
-		singlefile_galaxyPaths <- args$singlefile_galaxyPathNegative
-		singlefile_sampleNames <- args$singlefile_sampleNameNegative
-	}
-	if (exists("singlefile_galaxyPaths")) {
-		singlefile_galaxyPaths <- unlist(strsplit(singlefile_galaxyPaths, ","))
-		singlefile_sampleNames <- unlist(strsplit(singlefile_sampleNames, ","))
+  if (!is.null(args$singlefile_galaxyPath)) {
+    singlefile_galaxyPaths <- args$singlefile_galaxyPath
+    singlefile_sampleNames <- args$singlefile_sampleName
+  }
+  if (!is.null(args$singlefile_galaxyPathPositive)) {
+    singlefile_galaxyPaths <- args$singlefile_galaxyPathPositive
+    singlefile_sampleNames <- args$singlefile_sampleNamePositive
+  }
+  if (!is.null(args$singlefile_galaxyPathNegative)) {
+    singlefile_galaxyPaths <- args$singlefile_galaxyPathNegative
+    singlefile_sampleNames <- args$singlefile_sampleNameNegative
+  }
+  if (exists("singlefile_galaxyPaths")) {
+    singlefile_galaxyPaths <- unlist(strsplit(singlefile_galaxyPaths, ","))
+    singlefile_sampleNames <- unlist(strsplit(singlefile_sampleNames, ","))
 
-		singlefile <- NULL
-		for (singlefile_galaxyPath_i in seq_len(length(singlefile_galaxyPaths))) {
-			singlefile_galaxyPath <- singlefile_galaxyPaths[singlefile_galaxyPath_i]
-			singlefile_sampleName <- singlefile_sampleNames[singlefile_galaxyPath_i]
-			singlefile[[singlefile_sampleName]] <- singlefile_galaxyPath
-		}
-	}
-	for (argument in c(
-			   "zipfile", "zipfilePositive", "zipfileNegative",
-			   "singlefile_galaxyPath", "singlefile_sampleName",
-			   "singlefile_galaxyPathPositive", "singlefile_sampleNamePositive",
-			   "singlefile_galaxyPathNegative", "singlefile_sampleNameNegative"
-			   )) {
-		args[[argument]] <- NULL
-	}
-	return(list(zipfile = zipfile, singlefile = singlefile, args = args))
+    singlefile <- NULL
+    for (singlefile_galaxyPath_i in seq_len(length(singlefile_galaxyPaths))) {
+      singlefile_galaxyPath <- singlefile_galaxyPaths[singlefile_galaxyPath_i]
+      singlefile_sampleName <- singlefile_sampleNames[singlefile_galaxyPath_i]
+      singlefile[[singlefile_sampleName]] <- singlefile_galaxyPath
+    }
+  }
+  for (argument in c(
+    "zipfile", "zipfilePositive", "zipfileNegative",
+    "singlefile_galaxyPath", "singlefile_sampleName",
+    "singlefile_galaxyPathPositive", "singlefile_sampleNamePositive",
+    "singlefile_galaxyPathNegative", "singlefile_sampleNameNegative"
+  )) {
+    args[[argument]] <- NULL
+  }
+  return(list(zipfile = zipfile, singlefile = singlefile, args = args))
 }
 
 if (!exists("xdata")) stop("\n\nERROR: The RData doesn't contain any object called 'xdata'. This RData should have been created by an old version of XMCS 2.*")
 
-#Verification of a group step before doing the fillpeaks job.
+# Verification of a group step before doing the fillpeaks job.
 if (!hasFeatures(xdata)) stop("You must always do a group step after a retcor. Otherwise it won't work for the groupFWHM step")
 
 # Save arguments to generate a report
@@ -113,33 +113,33 @@ args <- rawFilePath$args
 
 # Function to retrieve the raw file in the working directory
 retrieveRawfileInTheWorkingDir <- function(singlefile, zipfile) {
-	if (!is.null(singlefile) && (length(singlefile) > 0)) {
-		for (singlefile_sampleName in names(singlefile)) {
-			singlefile_galaxyPath <- singlefile[[singlefile_sampleName]]
-			if (!file.exists(singlefile_galaxyPath)) {
-				error_message <- paste("Cannot access the sample:", singlefile_sampleName, "located:", singlefile_galaxyPath)
-				print(error_message)
-				stop(error_message)
-			}
-			file.symlink(singlefile_galaxyPath, singlefile_sampleName)
-		}
-		directory <- "."
-	}
-	if (!is.null(zipfile) && (zipfile != "")) {
-		if (!file.exists(zipfile)) {
-			error_message <- paste("Cannot access the Zip file:", zipfile)
-			print(error_message)
-			stop(error_message)
-		}
-		suppressWarnings(unzip(zipfile, unzip = "unzip"))
-		filesInZip <- unzip(zipfile, list = TRUE)
-		directories <- unique(unlist(lapply(strsplit(filesInZip$Name, "/"), function(x) x[1])))
-		directories <- directories[!(directories %in% c("__MACOSX")) & file.info(directories)$isdir]
-		directory <- "."
-		if (length(directories) == 1) directory <- directories
-		cat("files_root_directory\t", directory, "\n")
-	}
-	return(directory)
+  if (!is.null(singlefile) && (length(singlefile) > 0)) {
+    for (singlefile_sampleName in names(singlefile)) {
+      singlefile_galaxyPath <- singlefile[[singlefile_sampleName]]
+      if (!file.exists(singlefile_galaxyPath)) {
+        error_message <- paste("Cannot access the sample:", singlefile_sampleName, "located:", singlefile_galaxyPath)
+        print(error_message)
+        stop(error_message)
+      }
+      file.symlink(singlefile_galaxyPath, singlefile_sampleName)
+    }
+    directory <- "."
+  }
+  if (!is.null(zipfile) && (zipfile != "")) {
+    if (!file.exists(zipfile)) {
+      error_message <- paste("Cannot access the Zip file:", zipfile)
+      print(error_message)
+      stop(error_message)
+    }
+    suppressWarnings(unzip(zipfile, unzip = "unzip"))
+    filesInZip <- unzip(zipfile, list = TRUE)
+    directories <- unique(unlist(lapply(strsplit(filesInZip$Name, "/"), function(x) x[1])))
+    directories <- directories[!(directories %in% c("__MACOSX")) & file.info(directories)$isdir]
+    directory <- "."
+    if (length(directories) == 1) directory <- directories
+    cat("files_root_directory\t", directory, "\n")
+  }
+  return(directory)
 }
 
 # Retrieve files
@@ -170,17 +170,17 @@ xa <- groupFWHM(xsa, sigma = as.numeric(args$sigma), perfwhm = as.numeric(args$p
 peakList <- getPeaklist(xa, intval = args$intval)
 
 # Generate group names with and without decimals for mz and rt
-names_default <- groupnames(xa@xcmsSet, mzdec = 0, rtdec = 0)					# Names without decimals
-names_custom <- groupnames(xa@xcmsSet, mzdec = args$numDigitsMZ , rtdec = args$numDigitsRT)	# Names with "x" decimals
+names_default <- groupnames(xa@xcmsSet, mzdec = 0, rtdec = 0) # Names without decimals
+names_custom <- groupnames(xa@xcmsSet, mzdec = args$numDigitsMZ, rtdec = args$numDigitsRT) # Names with "x" decimals
 
 # Calculate indices of the columns to include from peakList
 # Select all columns except the last sample-specific columns
 ncols <- length(colnames(peakList))
-sample_cols <- length(xa@sample)   # Number of samples
+sample_cols <- length(xa@sample) # Number of samples
 
 # Indices for the columns of interest
-main_cols <- 1:(ncols - (3 + sample_cols))  # Main columns before sample columns
-tail_cols <- (ncols - 2):ncols              # The last 3 columns (adduct, isotope, pcgroup)
+main_cols <- 1:(ncols - (3 + sample_cols)) # Main columns before sample columns
+tail_cols <- (ncols - 2):ncols # The last 3 columns (adduct, isotope, pcgroup)
 
 # Combine the selected columns from matgrp with the group names
 variableMetadata <- cbind(
@@ -192,8 +192,8 @@ variableMetadata <- cbind(
 if (!exists("RTinMinute")) RTinMinute <- FALSE
 
 if (args$convertRTMinute && RTinMinute == FALSE) {
-	RTinMinute <- TRUE
-	variableMetadata <- RTSecondToMinute(variableMetadata = variableMetadata, convertRTMinute = args$convertRTMinute)
+  RTinMinute <- TRUE
+  variableMetadata <- RTSecondToMinute(variableMetadata = variableMetadata, convertRTMinute = args$convertRTMinute)
 }
 
 # Save the peak list to a TSV file

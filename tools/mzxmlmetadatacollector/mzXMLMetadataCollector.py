902ef60ff8e949595908b6bda093021ee548be49
Quentin Ruin <quentin.ruin@inrae.fr>
2024-12-23 16:09:38 +0100

    Update mzXMLMetadataCollector.py

--- tools/mzxmlmetadatacollector/mzXMLMetadataCollector.py
+++ tools/mzxmlmetadatacollector/mzXMLMetadataCollector.py
@@ -234,8 +234,8 @@ def get_info(file, format1):
                     line = f.readline()
                     if ('defaultArrayLength=' in line):
                         nbpoints1 = nbpoints1 + int(line
-                                    .split('defaultArrayLength="')[1]
-                                    .split('"')[0])
+                                                    .split('defaultArrayLength="')[1]
+                                                    .split('"')[0])
             f.close()
         except Exception:
             nbpoints1 = "Calculation failed"

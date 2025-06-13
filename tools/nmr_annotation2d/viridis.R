b6b7918fc6a7ae68dd6e7119a984dc5e5c2a9fbd
mtremblayfr <marie.tremblay-franco@toulouse.inra.fr>
2025-06-13 08:23:18 +0000

    Bugs

--- tools/nmr_annotation2d/viridis.R
+++ tools/nmr_annotation2d/viridis.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 viridis <- function(n, alpha = 1, begin = 0, end = 1, direction = 1, option = "D") {
   if (begin < 0 | begin > 1 | end < 0 | end > 1) {
     stop("begin and end must be in [0,1]")
@@ -25,43 +24,3 @@ viridis <- function(n, alpha = 1, begin = 0, end = 1, direction = 1, option = "D
   cols <- fn_cols(seq(begin, end, length.out = n)) / 255
   grDevices::rgb(cols[, 1], cols[, 2], cols[, 3], alpha = alpha)
 }
-=======
-viridis <- function(n, alpha = 1, begin = 0, end = 1, direction = 1, option = "D") {
-    if (begin < 0 | begin > 1 | end < 0 | end > 1) {
-        stop("begin and end must be in [0,1]")
-    }
-    if (abs(direction) != 1) {
-        stop("direction must be 1 or -1")
-    }
-    if (direction == -1) {
-        tmp <- begin
-        begin <- end
-        end <- tmp
-    }
-    option <- switch(EXPR = option,
-        A = "A",
-        magma = "A",
-        B = "B",
-        inferno = "B",
-        C = "C",
-        plasma = "C",
-        D = "D",
-        viridis = "D",
-        E = "E",
-        cividis = "E",
-        {
-            warning(paste0("Option '", option, "' does not exist. Defaulting to 'viridis'."))
-            "D"
-        }
-    )
-    map <- viridisLite::viridis.map[viridisLite::viridis.map$opt ==
-        option, ]
-    map_cols <- grDevices::rgb(map$R, map$G, map$B)
-    fn_cols <- grDevices::colorRamp(map_cols,
-        space = "Lab",
-        interpolate = "spline"
-    )
-    cols <- fn_cols(seq(begin, end, length.out = n)) / 255
-    grDevices::rgb(cols[, 1], cols[, 2], cols[, 3], alpha = alpha)
-}
->>>>>>> bb7574b871cff739f7abbda62a5269ec1f98971f

d0333f831c440bea1cecf296c0f659c30436439d
hechth <helge.hecht@recetox.muni.cz>
2025-07-10 09:39:48 +0200

    changed idnentation to 4

--- tools/checkformat/checkformat_script.R
+++ tools/checkformat/checkformat_script.R
@@ -10,182 +10,182 @@ readAndCheckF <- function(datFilC = "dataMatrix.tsv",
                           samFilC = "sampleMetadata.tsv",
                           varFilC = "variableMetadata.tsv",
                           makNamL) {
-  ## options
+    ## options
 
-  optStrAsFacL <- options()[["stringsAsFactors"]]
-  options(stringsAsFactors = FALSE)
+    optStrAsFacL <- options()[["stringsAsFactors"]]
+    options(stringsAsFactors = FALSE)
 
 
-  ## checking that the tables have no duplicated row or column names
+    ## checking that the tables have no duplicated row or column names
 
-  for (tabC in c("dat", "sam", "var")) {
-    tabNamC <- switch(tabC,
-      dat = "dataMatrix",
-      sam = "sampleMetadata",
-      var = "variableMetadata"
+    for (tabC in c("dat", "sam", "var")) {
+        tabNamC <- switch(tabC,
+            dat = "dataMatrix",
+            sam = "sampleMetadata",
+            var = "variableMetadata"
+        )
+
+        rowVc <- read.table(eval(parse(text = paste0(tabC, "FilC"))),
+            check.names = FALSE,
+            header = TRUE,
+            sep = "\t"
+        )[, 1]
+
+        colVc <- unlist(read.table(eval(parse(text = paste0(tabC, "FilC"))),
+            check.names = FALSE,
+            nrow = 1,
+            sep = "\t"
+        ))[-1]
+
+        if (any(duplicated(rowVc))) {
+            stop("The following row name(s) is/are duplicated in the ",
+                tabNamC,
+                " table: '",
+                paste(rowVc[duplicated(rowVc)], collapse = "', '"), "'",
+                call. = FALSE
+            )
+        }
+
+        if (any(duplicated(colVc))) {
+            stop("The following column name(s) is/are duplicated in the ",
+                tabNamC,
+                " table: '",
+                paste(colVc[duplicated(colVc)], collapse = "', '"), "'",
+                call. = FALSE
+            )
+        }
+    }
+
+
+    ## reading tables
+
+    datMN <- t(as.matrix(read.table(datFilC,
+        check.names = FALSE,
+        header = TRUE,
+        row.names = 1,
+        sep = "\t"
+    )))
+
+    samDF <- read.table(samFilC,
+        check.names = FALSE,
+        header = TRUE,
+        row.names = 1,
+        sep = "\t"
     )
 
-    rowVc <- read.table(eval(parse(text = paste0(tabC, "FilC"))),
-      check.names = FALSE,
-      header = TRUE,
-      sep = "\t"
-    )[, 1]
-
-    colVc <- unlist(read.table(eval(parse(text = paste0(tabC, "FilC"))),
-      check.names = FALSE,
-      nrow = 1,
-      sep = "\t"
-    ))[-1]
-
-    if (any(duplicated(rowVc))) {
-      stop("The following row name(s) is/are duplicated in the ",
-        tabNamC,
-        " table: '",
-        paste(rowVc[duplicated(rowVc)], collapse = "', '"), "'",
-        call. = FALSE
-      )
-    }
-
-    if (any(duplicated(colVc))) {
-      stop("The following column name(s) is/are duplicated in the ",
-        tabNamC,
-        " table: '",
-        paste(colVc[duplicated(colVc)], collapse = "', '"), "'",
-        call. = FALSE
-      )
-    }
-  }
-
-
-  ## reading tables
-
-  datMN <- t(as.matrix(read.table(datFilC,
-    check.names = FALSE,
-    header = TRUE,
-    row.names = 1,
-    sep = "\t"
-  )))
-
-  samDF <- read.table(samFilC,
-    check.names = FALSE,
-    header = TRUE,
-    row.names = 1,
-    sep = "\t"
-  )
-
-  varDF <- read.table(varFilC,
-    check.names = FALSE,
-    header = TRUE,
-    row.names = 1,
-    sep = "\t"
-  )
-
-
-  ## checking that dataMatrix is numeric and that the sample and variable numbers are coherent
-
-  if (mode(datMN) != "numeric") {
-    stop("The dataMatrix is not of the 'numeric' type",
-      call. = FALSE
+    varDF <- read.table(varFilC,
+        check.names = FALSE,
+        header = TRUE,
+        row.names = 1,
+        sep = "\t"
     )
-  }
 
-  if (nrow(datMN) != nrow(samDF)) {
-    if (nrow(datMN) > nrow(samDF)) {
-      print(setdiff(rownames(datMN), rownames(samDF)))
-      stop("The sample names above from dataMatrix were not found in sampleMetadata",
-        call. = FALSE
-      )
-    } else {
-      print(setdiff(rownames(samDF), rownames(datMN)))
-      stop("The sample names above from sampleMetadata were not found in dataMatrix",
-        call. = FALSE
-      )
+
+    ## checking that dataMatrix is numeric and that the sample and variable numbers are coherent
+
+    if (mode(datMN) != "numeric") {
+        stop("The dataMatrix is not of the 'numeric' type",
+            call. = FALSE
+        )
     }
-  }
 
-  if (ncol(datMN) != nrow(varDF)) {
-    if (ncol(datMN) > nrow(varDF)) {
-      print(setdiff(colnames(datMN), rownames(varDF)))
-      stop("The variable names above from dataMatrix were not found in variableMetadata",
-        call. = FALSE
-      )
-    } else {
-      print(setdiff(rownames(varDF), colnames(datMN)))
-      stop("The variable names above from variableMetadata were not found in dataMatrix",
-        call. = FALSE
-      )
+    if (nrow(datMN) != nrow(samDF)) {
+        if (nrow(datMN) > nrow(samDF)) {
+            print(setdiff(rownames(datMN), rownames(samDF)))
+            stop("The sample names above from dataMatrix were not found in sampleMetadata",
+                call. = FALSE
+            )
+        } else {
+            print(setdiff(rownames(samDF), rownames(datMN)))
+            stop("The sample names above from sampleMetadata were not found in dataMatrix",
+                call. = FALSE
+            )
+        }
     }
-  }
 
-
-  ## making sample and variable names (optional)
-
-  newL <- FALSE
-
-  if (makNamL) {
-    cat("\n\nMessage: Converting sample and variable names to the standard R format\n")
-
-    rownames(datMN) <- make.names(rownames(datMN), unique = TRUE)
-    colnames(datMN) <- make.names(colnames(datMN), unique = TRUE)
-    rownames(samDF) <- make.names(rownames(samDF), unique = TRUE)
-    rownames(varDF) <- make.names(rownames(varDF), unique = TRUE)
-
-    newL <- TRUE
-  }
-
-
-  ## checking sample and variable names
-
-  chkL <- TRUE
-
-  if (!identical(rownames(datMN), rownames(samDF))) {
-    if (identical(sort(rownames(datMN)), sort(rownames(samDF)))) {
-      cat("\n\nMessage: Re-ordering dataMatrix sample names to match sampleMetadata\n")
-      datMN <- datMN[rownames(samDF), , drop = FALSE]
-
-      stopifnot(identical(sort(rownames(datMN)), sort(rownames(samDF))))
-
-      newL <- TRUE
-    } else {
-      cat("\n\nStop: The sample names of dataMatrix and sampleMetadata do not match:\n")
-      print(cbind.data.frame(
-        indice = 1:nrow(datMN),
-        dataMatrix = rownames(datMN),
-        sampleMetadata = rownames(samDF)
-      )[rownames(datMN) != rownames(samDF), , drop = FALSE])
-      chkL <- FALSE
+    if (ncol(datMN) != nrow(varDF)) {
+        if (ncol(datMN) > nrow(varDF)) {
+            print(setdiff(colnames(datMN), rownames(varDF)))
+            stop("The variable names above from dataMatrix were not found in variableMetadata",
+                call. = FALSE
+            )
+        } else {
+            print(setdiff(rownames(varDF), colnames(datMN)))
+            stop("The variable names above from variableMetadata were not found in dataMatrix",
+                call. = FALSE
+            )
+        }
     }
-  }
 
-  if (!identical(colnames(datMN), rownames(varDF))) {
-    if (identical(sort(colnames(datMN)), sort(rownames(varDF)))) {
-      cat("\n\nMessage: Re-ordering dataMatrix variable names to match variableMetadata\n")
-      datMN <- datMN[, rownames(varDF), drop = FALSE]
 
-      stopifnot(identical(sort(colnames(datMN)), sort(rownames(varDF))))
+    ## making sample and variable names (optional)
 
-      newL <- TRUE
-    } else {
-      cat("\n\nStop: The variable names of dataMatrix and variableMetadata do not match:\n")
-      print(cbind.data.frame(
-        indice = 1:ncol(datMN),
-        dataMatrix = colnames(datMN),
-        variableMetadata = rownames(varDF)
-      )[colnames(datMN) != rownames(varDF), , drop = FALSE])
-      chkL <- FALSE
+    newL <- FALSE
+
+    if (makNamL) {
+        cat("\n\nMessage: Converting sample and variable names to the standard R format\n")
+
+        rownames(datMN) <- make.names(rownames(datMN), unique = TRUE)
+        colnames(datMN) <- make.names(colnames(datMN), unique = TRUE)
+        rownames(samDF) <- make.names(rownames(samDF), unique = TRUE)
+        rownames(varDF) <- make.names(rownames(varDF), unique = TRUE)
+
+        newL <- TRUE
     }
-  }
 
 
-  options(stringsAsFactors = optStrAsFacL)
+    ## checking sample and variable names
 
-  resLs <- list(
-    chkL = chkL,
-    newL = newL,
-    datMN = datMN,
-    samDF = samDF,
-    varDF = varDF
-  )
+    chkL <- TRUE
 
-  return(resLs)
+    if (!identical(rownames(datMN), rownames(samDF))) {
+        if (identical(sort(rownames(datMN)), sort(rownames(samDF)))) {
+            cat("\n\nMessage: Re-ordering dataMatrix sample names to match sampleMetadata\n")
+            datMN <- datMN[rownames(samDF), , drop = FALSE]
+
+            stopifnot(identical(sort(rownames(datMN)), sort(rownames(samDF))))
+
+            newL <- TRUE
+        } else {
+            cat("\n\nStop: The sample names of dataMatrix and sampleMetadata do not match:\n")
+            print(cbind.data.frame(
+                indice = 1:nrow(datMN),
+                dataMatrix = rownames(datMN),
+                sampleMetadata = rownames(samDF)
+            )[rownames(datMN) != rownames(samDF), , drop = FALSE])
+            chkL <- FALSE
+        }
+    }
+
+    if (!identical(colnames(datMN), rownames(varDF))) {
+        if (identical(sort(colnames(datMN)), sort(rownames(varDF)))) {
+            cat("\n\nMessage: Re-ordering dataMatrix variable names to match variableMetadata\n")
+            datMN <- datMN[, rownames(varDF), drop = FALSE]
+
+            stopifnot(identical(sort(colnames(datMN)), sort(rownames(varDF))))
+
+            newL <- TRUE
+        } else {
+            cat("\n\nStop: The variable names of dataMatrix and variableMetadata do not match:\n")
+            print(cbind.data.frame(
+                indice = 1:ncol(datMN),
+                dataMatrix = colnames(datMN),
+                variableMetadata = rownames(varDF)
+            )[colnames(datMN) != rownames(varDF), , drop = FALSE])
+            chkL <- FALSE
+        }
+    }
+
+
+    options(stringsAsFactors = optStrAsFacL)
+
+    resLs <- list(
+        chkL = chkL,
+        newL = newL,
+        datMN = datMN,
+        samDF = samDF,
+        varDF = varDF
+    )
+
+    return(resLs)
 } ## end of checkAndReadF
--- tools/checkformat/checkformat_wrapper.R
+++ tools/checkformat/checkformat_wrapper.R
@@ -3,9 +3,9 @@
 library(batch) ## parseCommandArgs
 
 source_local <- function(fname) {
-  argv <- commandArgs(trailingOnly = FALSE)
-  base_dir <- dirname(substring(argv[grep("--file=", argv)], 8))
-  source(paste(base_dir, fname, sep = "/"))
+    argv <- commandArgs(trailingOnly = FALSE)
+    base_dir <- dirname(substring(argv[grep("--file=", argv)], 8))
+    source(paste(base_dir, fname, sep = "/"))
 }
 
 source_local("checkformat_script.R")
@@ -34,8 +34,8 @@ modNamC <- "Check Format" ## module name
 sink(argVc["information"])
 
 cat("\nStart of the '", modNamC, "' Galaxy module call: ",
-  format(Sys.time(), "%a %d %b %Y %X"), "\n",
-  sep = ""
+    format(Sys.time(), "%a %d %b %Y %X"), "\n",
+    sep = ""
 )
 
 
@@ -45,10 +45,10 @@ cat("\nStart of the '", modNamC, "' Galaxy module call: ",
 
 
 resLs <- readAndCheckF(
-  argVc["dataMatrix_in"],
-  argVc["sampleMetadata_in"],
-  argVc["variableMetadata_in"],
-  as.logical(argVc["makeNameL"])
+    argVc["dataMatrix_in"],
+    argVc["sampleMetadata_in"],
+    argVc["variableMetadata_in"],
+    as.logical(argVc["makeNameL"])
 )
 
 
@@ -61,55 +61,55 @@ resLs <- readAndCheckF(
 
 datMN <- resLs[["datMN"]]
 datDF <- cbind.data.frame(
-  dataMatrix = colnames(datMN),
-  as.data.frame(t(datMN))
+    dataMatrix = colnames(datMN),
+    as.data.frame(t(datMN))
 )
 write.table(datDF,
-  file = argVc[["dataMatrix_out"]],
-  quote = FALSE,
-  row.names = FALSE,
-  sep = "\t"
+    file = argVc[["dataMatrix_out"]],
+    quote = FALSE,
+    row.names = FALSE,
+    sep = "\t"
 )
 
 ## sampleMetadata
 
 samDF <- resLs[["samDF"]]
 samDF <- cbind.data.frame(
-  sampleMetadata = rownames(samDF),
-  samDF
+    sampleMetadata = rownames(samDF),
+    samDF
 )
 write.table(samDF,
-  file = argVc["sampleMetadata_out"],
-  quote = FALSE,
-  row.names = FALSE,
-  sep = "\t"
+    file = argVc["sampleMetadata_out"],
+    quote = FALSE,
+    row.names = FALSE,
+    sep = "\t"
 )
 
 ## variableMetadata
 
 varDF <- resLs[["varDF"]]
 varDF <- cbind.data.frame(
-  variableMetadata = rownames(varDF),
-  varDF
+    variableMetadata = rownames(varDF),
+    varDF
 )
 write.table(varDF,
-  file = argVc["variableMetadata_out"],
-  quote = FALSE,
-  row.names = FALSE,
-  sep = "\t"
+    file = argVc["variableMetadata_out"],
+    quote = FALSE,
+    row.names = FALSE,
+    sep = "\t"
 )
 
 if (resLs[["chkL"]]) {
-  if (resLs[["newL"]]) {
-    cat("\nWarning: The sample and/or variable names or orders from the input tables have been modified\n(see the information file for details); please use the new output tables for your analyses.\n")
-  } else {
-    cat("\nThe input tables have a correct format and can be used for your analyses.\n")
-  }
+    if (resLs[["newL"]]) {
+        cat("\nWarning: The sample and/or variable names or orders from the input tables have been modified\n(see the information file for details); please use the new output tables for your analyses.\n")
+    } else {
+        cat("\nThe input tables have a correct format and can be used for your analyses.\n")
+    }
 }
 
 cat("\nEnd of the '", modNamC, "' Galaxy module call: ",
-  format(Sys.time(), "%a %d %b %Y %X"), "\n",
-  sep = ""
+    format(Sys.time(), "%a %d %b %Y %X"), "\n",
+    sep = ""
 )
 
 cat("\n\n\n============================================================================")
@@ -122,12 +122,12 @@ sessioninfo <- sessionInfo()
 cat(sessioninfo$R.version$version.string, "\n")
 cat("Main packages:\n")
 for (pkg in names(sessioninfo$otherPkgs)) {
-  cat(paste(pkg, packageVersion(pkg)), "\t")
+    cat(paste(pkg, packageVersion(pkg)), "\t")
 }
 cat("\n")
 cat("Other loaded packages:\n")
 for (pkg in names(sessioninfo$loadedOnly)) {
-  cat(paste(pkg, packageVersion(pkg)), "\t")
+    cat(paste(pkg, packageVersion(pkg)), "\t")
 }
 cat("\n")
 
@@ -136,7 +136,7 @@ cat("===========================================================================
 sink()
 
 if (!resLs[["chkL"]]) {
-  stop("Please check the generated 'information' file")
+    stop("Please check the generated 'information' file")
 }
 
 

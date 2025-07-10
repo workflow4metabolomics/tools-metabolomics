1fd4d2bd142f966d297a651487edd967548bb5b1
hechth <helge.hecht@recetox.muni.cz>
2025-07-10 11:31:41 +0200

    deleted no longer needed files

--- tools/tablemerge/planemo_test.sh
+++
@@ -1,3 +0,0 @@
-planemo conda_init --conda_prefix /tmp/mc/
-planemo conda_install --conda_prefix /tmp/mc/ .
-planemo test --install_galaxy --conda_dependency_resolution --conda_prefix /tmp/mc/ --no_cleanup

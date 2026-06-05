b284161a83045a59acc1d69d6bf97b11079e72df
mtremblayfr <marie.tremblay-franco@toulouse.inrae.fr>
2026-06-05 07:17:37 +0200

    Linting of Rscripts and requested changes for PR

--- tools/nmr_preprocessing/ReadFids_wrapper.R
+++ tools/nmr_preprocessing/ReadFids_wrapper.R
@@ -14,24 +14,24 @@ sink(log_out, append = TRUE, split = TRUE)
 # Command line interface (optparse)
 # ------------------------------
 option_list <- list(
-    make_option(c("-f", "--fidzipfile"),
-        type = "character",
-        help = "Path to zipped Bruker FID file"
-    ),
-    make_option(c("-t", "--title_line"),
-        type = "character",
-        help = "Title line for output"
-    ),
-    make_option(c("-s", "--subdirectories"),
-        action = "store_true",
-        dest = "subdirectories", default = FALSE,
-        help = "Whether to use subdirectories (boolean flag)"
-    ),
-    make_option(c("-d", "--dirs_names"),
-        action = "store_true",
-        dest = "dirs_names", default = FALSE,
-        help = "Whether to use dirs_names (boolean flag)"
-    )
+  make_option(c("-f", "--fidzipfile"),
+    type = "character",
+    help = "Path to zipped Bruker FID file"
+  ),
+  make_option(c("-t", "--title_line"),
+    type = "character",
+    help = "Title line for output"
+  ),
+  make_option(c("-s", "--subdirectories"),
+    action = "store_true",
+    dest = "subdirectories", default = FALSE,
+    help = "Whether to use subdirectories (boolean flag)"
+  ),
+  make_option(c("-d", "--dirs_names"),
+    action = "store_true",
+    dest = "dirs_names", default = FALSE,
+    help = "Whether to use dirs_names (boolean flag)"
+  )
 )
 
 opt_parser <- OptionParser(option_list = option_list)
@@ -60,28 +60,28 @@ dirs_names <- args$dirs_names
 ## Computation
 ## ======================================================
 if (length(warnings()) > 0) { # or !is.null(warnings())
-    print("something happened")
+  print("something happened")
 }
 
 ## Starting
 cat("\nStart of 'ReadFids' Galaxy module call: ",
-    as.character(Sys.time()), "\n\n",
-    sep = ""
+  as.character(Sys.time()), "\n\n",
+  sep = ""
 )
 
 outputs <- read_fids(
-    path = path, l = l, subdirs = subdirs,
-    dirs_names = dirs_names
+  path = path, l = l, subdirs = subdirs,
+  dirs_names = dirs_names
 )
 data_matrix <- outputs[["fid_data"]] # Data matrix
 data_sample <- outputs[["fid_info"]] # Sample metadata
 
 pdf(nom_graphe, onefile = TRUE, width = 13, height = 13)
 draw_signal(data_matrix,
-    subtype = "stacked",
-    re_im_mod_arg = c(TRUE, FALSE, FALSE, FALSE), vertical = TRUE,
-    xlab = "Frequency", num_stacked = 4, main = "Raw FID data",
-    create_window_drawsignal = FALSE
+  subtype = "stacked",
+  re_im_mod_arg = c(TRUE, FALSE, FALSE, FALSE), vertical = TRUE,
+  xlab = "Frequency", num_stacked = 4, main = "Raw FID data",
+  create_window_drawsignal = FALSE
 )
 invisible(dev.off())
 
@@ -96,14 +96,14 @@ print(dim(data_sample))
 
 # Data matrix
 write.table(data_matrix,
-    file = datamatrix, quote = FALSE, row.names = TRUE,
-    sep = "\t", col.names = TRUE
+  file = datamatrix, quote = FALSE, row.names = TRUE,
+  sep = "\t", col.names = TRUE
 )
 
 # Sample metadata
 write.table(data_sample,
-    file = samplemetadata, quote = FALSE, row.names = TRUE,
-    sep = "\t", col.names = TRUE
+  file = samplemetadata, quote = FALSE, row.names = TRUE,
+  sep = "\t", col.names = TRUE
 )
 
 # input arguments
@@ -114,8 +114,6 @@ print(sessionInfo())
 ## Ending
 
 cat("\nEnd of 'ReadFids' Galaxy module call: ",
-    as.character(Sys.time()),
-    sep = ""
-)
+    as.character(Sys.time()), sep = "")
 
 sink()
